0b87ec1b5894d5adc8bb32ace10d107f569c557f
STEVENSONSJ22 <StevensonSJ22@gcc.edu>
2023-03-01 13:56:56 -0500

    Updated main: Added a prompt asking for users name, then outputting "Hello, *name*".

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner scan = new Scanner(System.in);
+        String name = scan.next();
+        System.out.println("Hello " + name);
     }
 }
